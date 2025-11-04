460c8f7be2bd5308fe0e79898a51c4cd72e1fe76
Arom Sanchez <arom.sanchez@tecsup.edu.pe>
2025-11-04 10:26:39 -0500

    aaaasdas

--- foros_proyecto/settings.py
+++ foros_proyecto/settings.py
@@ -92,11 +92,18 @@ WSGI_APPLICATION = 'foros_proyecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-}
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
+    }
+else:
+    # Base de datos local temporal (no afecta a Render)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
